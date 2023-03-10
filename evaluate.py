634975fcd5cbd757dfbf904d2cb61bfc188da234
lcmp101 <lucia.martin.perez@alumnos.upm.es>
2023-03-10 16:27:45 +0100

    Updated package list and created python scripts from jupyter notebooks

--- evaluate.py
+++ evaluate.py
@@ -1,8 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 import sys
 import os
+#print(os.environ['LD_LIBRARY_PATH'])
+os.environ['LD_LIBRARY_PATH']='/data/lmp/anaconda3/envs/siamese-mask-rcnn/lib/'
+print(os.environ['LD_LIBRARY_PATH'])
 
 import tensorflow as tf
-
 tf.logging.set_verbosity(tf.logging.INFO)
 sess_config = tf.ConfigProto()
 
@@ -11,16 +16,17 @@ MASK_RCNN_MODEL_PATH = 'lib/Mask_RCNN/'
 
 if MASK_RCNN_MODEL_PATH not in sys.path:
     sys.path.append(MASK_RCNN_MODEL_PATH)
-
+    
 from samples.coco import coco
-from mrcnn import utils
+from mrcnn import utils# In[1]:
+
 from mrcnn import model as modellib
 from mrcnn import visualize
-
+    
 from lib import utils as siamese_utils
 from lib import model as siamese_model
 from lib import config as siamese_config
-
+   
 import time
 import datetime
 import random
@@ -36,3 +42,189 @@ ROOT_DIR = os.getcwd()
 
 # Directory to save logs and trained model
 MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+
+# ### Dataset
+
+# In[4]:
+
+
+# train_classes = coco_nopascal_classes
+train_classes = np.array(range(1,81))
+
+
+# In[5]:
+
+
+# Load COCO/val dataset
+coco_val = siamese_utils.IndexedCocoDataset()
+coco_object = coco_val.load_coco(COCO_DATA, "val", year="2017", return_coco=True)
+coco_val.prepare()
+coco_val.build_indices()
+coco_val.ACTIVE_CLASSES = train_classes
+
+
+# ### Model
+
+# In[6]:
+
+
+class SmallEvalConfig(siamese_config.Config):
+    # Set batch size to 1 since we'll be running inference on
+    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+    NUM_CLASSES = 1 + 1
+    NAME = 'coco'
+    EXPERIMENT = 'evaluation'
+    CHECKPOINT_DIR = 'checkpoints/'
+    NUM_TARGETS = 1
+    
+class LargeEvalConfig(siamese_config.Config):
+    # Set batch size to 1 since we'll be running inference on
+    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+    NUM_CLASSES = 1 + 1
+    NAME = 'coco'
+    EXPERIMENT = 'evaluation'
+    CHECKPOINT_DIR = 'checkpoints/'
+    NUM_TARGETS = 1
+    
+    # Large image sizes
+    TARGET_MAX_DIM = 192
+    TARGET_MIN_DIM = 150
+    IMAGE_MIN_DIM = 800
+    IMAGE_MAX_DIM = 1024
+    # Large model size
+    FPN_CLASSIF_FC_LAYERS_SIZE = 1024
+    FPN_FEATUREMAPS = 256
+    # Large number of rois at all stages
+    RPN_ANCHOR_STRIDE = 1
+    RPN_TRAIN_ANCHORS_PER_IMAGE = 256
+    POST_NMS_ROIS_TRAINING = 2000
+    POST_NMS_ROIS_INFERENCE = 1000
+    TRAIN_ROIS_PER_IMAGE = 200
+    DETECTION_MAX_INSTANCES = 100
+    MAX_GT_INSTANCES = 100
+
+
+# #### Select small or large model config
+
+# In[7]:
+
+
+# The small model trains on a single GPU and runs much faster.
+# The large model is the same we used in our experiments but needs multiple GPUs and more time for training.
+model_size = 'small' # or 'large'
+
+
+# In[8]:
+
+
+if model_size == 'small':
+    config = SmallEvalConfig()
+elif model_size == 'large':
+    config = LargeEvalConfig()
+    
+config.display()
+
+
+# In[9]:
+
+
+# Provide training schedule of the model
+# When evaluationg intermediate steps the tranining schedule must be provided
+train_schedule = OrderedDict()
+if model_size == 'small':
+    train_schedule[1] = {"learning_rate": config.LEARNING_RATE, "layers": "heads"}
+    train_schedule[120] = {"learning_rate": config.LEARNING_RATE, "layers": "4+"}
+    train_schedule[160] = {"learning_rate": config.LEARNING_RATE/10, "layers": "all"}
+elif model_size == 'large':
+    train_schedule[1] = {"learning_rate": config.LEARNING_RATE, "layers": "heads"}
+    train_schedule[240] = {"learning_rate": config.LEARNING_RATE, "layers": "all"}
+    train_schedule[320] = {"learning_rate": config.LEARNING_RATE/10, "layers": "all"}
+
+
+# In[10]:
+
+
+# Select checkpoint
+if model_size == 'small':
+    checkpoint = 'checkpoints/small_siamese_mrcnn_0160.h5'
+elif model_size == 'large':
+    checkpoint = 'checkpoints/large_siamese_mrcnn_coco_full_0320.h5'
+
+
+# ### Evaluation
+
+# In[11]:
+
+
+# Load and evaluate model
+# Create model object in inference mode.
+model = siamese_model.SiameseMaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+model.load_checkpoint(checkpoint, training_schedule=train_schedule)
+# Evaluate only active classes
+active_class_idx = np.array(coco_val.ACTIVE_CLASSES) - 1
+
+# Evaluate on the validation set
+print('starting evaluation ...')
+siamese_utils.evaluate_dataset(model, coco_val, coco_object, eval_type="bbox", 
+                 dataset_type='coco', limit=1000, image_ids=None, 
+                 class_index=active_class_idx, verbose=1)
+
+
+# In[12]:
+
+
+config.NUM_TARGETS = 5
+# Load and evaluate model
+# Create model object in inference mode.
+model = siamese_model.SiameseMaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+model.load_checkpoint(checkpoint, training_schedule=train_schedule)
+# Evaluate only active classes
+active_class_idx = np.array(coco_val.ACTIVE_CLASSES) - 1
+
+# Evaluate on the validation set
+print('starting evaluation ...')
+siamese_utils.evaluate_dataset(model, coco_val, coco_object, eval_type="bbox", 
+                 dataset_type='coco', limit=1000, image_ids=None, 
+                 class_index=active_class_idx, verbose=1)
+
+
+# ### Visualization
+
+# In[13]:
+
+
+# Create model object in inference mode.
+model = siamese_model.SiameseMaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+model.load_checkpoint(checkpoint, training_schedule=train_schedule)
+
+
+# In[17]:
+
+
+# Select category
+category = 1
+image_id = np.random.choice(coco_val.category_image_index[category])   
+# Load target
+target = siamese_utils.get_one_target(category, coco_val, config)
+# Load image
+image = coco_val.load_image(image_id)
+print("image_id", image_id)
+
+
+# Run detection
+results = model.detect([[target]], [image], verbose=1)
+r = results[0]
+# Display results
+siamese_utils.display_results(target, image, r['rois'], r['masks'], r['class_ids'], r['scores'])
+
+
+# In[ ]:
+
+
+
+
